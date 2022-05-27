db1a480d618b498805af6dedff32b6ebfcdab215
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-05-27 15:03:35 +0200

    click handler and hover

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Dot from "./Dot";
 
 function App() {
+    
     return (
         <div className="dots-wrapper">
             {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
+                <Dot key={i} myNumber={i}/>
             ))}
         </div>
     );
--- src/Dot.js
+++ src/Dot.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dot.css';
 
-const Dot = () => {
-    // TODO: put your code here
+const Dot = props => {
+    const { myNumber } = props;
+    const [isVisible, setIsVisible] = useState(false);
+
+    const clickHandler = () => {
+        !isVisible && setIsVisible(true);
+    }
+
     return (
-        <div className="dot"/>
+        <div className="dot" onClick={clickHandler}>
+            {isVisible && <span> {myNumber}</span>}
+        </div>
     );
 };
 
