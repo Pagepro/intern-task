92b26d33c08b1a7c019e0018c417b9759d247055
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-05-27 15:29:54 +0200

    done all tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import Dot from "./Dot";
 
 function App() {
-    
+    const [activeNumber, setActiveNumber] = useState(null);
+    const setActiveNumberHandler = newNumber => {
+        activeNumber !== newNumber && setActiveNumber(newNumber);
+    }
     return (
         <div className="dots-wrapper">
             {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i} myNumber={i}/>
+                <Dot key={i} myNumber={i} activeNumber={activeNumber} setActiveNumberHandler={setActiveNumberHandler} />
             ))}
         </div>
     );
--- src/Dot.js
+++ src/Dot.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dot.css';
 import { generateRandomRGB } from './utils/utils.js';
 
 const Dot = props => {
-    const { myNumber } = props;
+    const { myNumber, activeNumber, setActiveNumberHandler } = props;
     const [isVisible, setIsVisible] = useState(false);
     const [backgroundColorArray, setBackgroundColorArray] = useState([0, 0, 0]);
 
     const clickHandler = () => {
         !isVisible && setIsVisible(true);
         setBackgroundColorArray(generateRandomRGB(backgroundColorArray));
+        setActiveNumberHandler(myNumber);
     }
+
+    useEffect(() => {
+        if (activeNumber !== myNumber && isVisible) {
+            setIsVisible(false);
+            setBackgroundColorArray([0, 0, 0]);
+        }
+    }, [activeNumber, myNumber, isVisible])
 
     return (
         <div className="dot" onClick={clickHandler}
