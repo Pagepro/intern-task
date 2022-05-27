aaf8671742398e92616df540c0ae009def476a5d
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-05-27 15:12:42 +0200

    backgroundColor dot as array RGB

--- src/Dot.js
+++ src/Dot.js
@@ -4,13 +4,19 @@ import './Dot.css';
 const Dot = props => {
     const { myNumber } = props;
     const [isVisible, setIsVisible] = useState(false);
+    const [backgroundColorArray, setBackgroundColorArray] = useState([0, 0, 0]);
 
     const clickHandler = () => {
         !isVisible && setIsVisible(true);
+        setBackgroundColorArray([8, 8, 8]);
     }
 
     return (
-        <div className="dot" onClick={clickHandler}>
+        <div className="dot" onClick={clickHandler}
+            style={{
+                backgroundColor: `rgb(${backgroundColorArray.join(',')})`
+            }}
+        >
             {isVisible && <span> {myNumber}</span>}
         </div>
     );
